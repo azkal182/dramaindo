5af3eab0ab51cf361bcb305cb0901ecaa4a25dc1
azka <mohazkalarif@gmail.com>
2023-01-03 20:31:55 +0700

    add search and movie

--- routes/api/v1/movie/index.js
+++ routes/api/v1/movie/index.js
@@ -4,9 +4,10 @@ module.exports = async function (fastify, opts) {
   fastify.get('/:mode', async function (request, reply) {
     const drama  = new Movie()
     const page = request.query.page ? request.query.page : 1
+    const tmdb = request.query.tmdb
     // console.log(page)
     const mode = request.params.mode
-    const data = await drama.index(mode, page)
+    const data = await drama.index(mode, page, tmdb ? true:false)
     return data
   })
 }
--- routes/api/v1/tv/index.js
+++ routes/api/v1/tv/index.js
@@ -4,9 +4,10 @@ module.exports = async function (fastify, opts) {
   fastify.get('/:mode', async function (request, reply) {
     const drama  = new Dramaindo()
     const page = request.query.page ? request.query.page : 1
+    const tmdb = request.query.tmdb ? true : false
     // console.log(page)
     const mode = request.params.mode
-    const data = await drama.index(mode, page)
+    const data = await drama.index(mode, page, tmdb)
     return data
   })
 }
