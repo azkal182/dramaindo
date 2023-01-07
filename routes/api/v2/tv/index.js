fc7be4cd90b5d28a7c0a0418252f5c871e0eea48
azka <mohazkalarif@gmail.com>
2023-01-07 18:17:04 +0700

    tes firebase

--- routes/api/v2/tv/index.js
+++ routes/api/v2/tv/index.js
@@ -49,4 +49,104 @@ module.exports = async function (fastify, opts) {
 
   //return 'oke'
  });
+ 
+ fastify.get("/popular", async function (request, res) {
+  try {
+   let response = [];
+
+   await db
+    .collection("series")
+    .orderBy("rating", "desc")
+    .limit(24)
+    .get()
+    .then((data) => {
+     let docs = data.docs;
+     docs.map((doc) => {
+      const selectedData = {
+       name: doc.data().title,
+       id: doc.data().id,
+       datePublished: doc.data().datePublished,
+       director: doc.data().director,
+       categories: doc.data().categories,
+       episode: doc.data().episode,
+       genres: doc.data().genres,
+       poster: doc.data().poster,
+       rating: doc.data().rating,
+       reviewCount: doc.data().reviewCount,
+       trailer: doc.data().trailer,
+       TMDB: doc.data().TMDB,
+      };
+
+      response.push(selectedData);
+     });
+
+     return response;
+    });
+   //console.log(response)
+   //return response;
+   return res.status(200).send({ status: "Success", length:response.length, results: response });
+  } catch (error) {
+   console.log(error);
+   res.status(500).send({ status: "Failed", msg: error });
+  }
+
+  //return 'oke'
+ });
+ 
+ fastify.get("/country/:country", async function (request, res) {
+  try {
+   let response = [];
+let query = []
+if (request.params.country == 'korea')
+{
+ query = ['Korea', 'South Korea']
+} else if (request.params.country == 'china')
+{
+ query = ['China']
+} else if (request.params.country == 'west')
+{
+ query = ['West']
+} else if (request.params.country == 'asian')
+{
+ query = ['Asian']
+}
+   await db
+    .collection("series")
+    .where('categories', 'array-contains-any', query)
+    
+    .limit(24)
+    .get()
+    .then((data) => {
+     let docs = data.docs;
+     docs.map((doc) => {
+      const selectedData = {
+       name: doc.data().title,
+       id: doc.data().id,
+       datePublished: doc.data().datePublished,
+       director: doc.data().director,
+       categories: doc.data().categories,
+       episode: doc.data().episode,
+       genres: doc.data().genres,
+       poster: doc.data().poster,
+       rating: doc.data().rating,
+       reviewCount: doc.data().reviewCount,
+       trailer: doc.data().trailer,
+       TMDB: doc.data().TMDB,
+      };
+
+      response.push(selectedData);
+     });
+
+     return response;
+    });
+   //console.log(response)
+   //return response;
+   return res.status(200).send({ status: "Success", length:response.length, results: response });
+  } catch (error) {
+   console.log(error);
+   res.status(500).send({ status: "Failed", msg: error });
+  }
+
+  //return 'oke'
+ });
 };
